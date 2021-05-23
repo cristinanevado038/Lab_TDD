3edfbb59571069442c2d1d4f1e707a5198c0abc7
cristina nevado delgado <cnevdel@gmail.com>
2021-05-23 12:38:06 +0200

    Se añade proyecto de Test correcto a la solución

--- Calculadora_TDD/Calculadora_TDD_Test/CalcTest.cs
+++ Calculadora_TDD/Calculadora_TDD_Test/CalcTest.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using NUnit.Framework;
+using Calculadora;
 
 namespace Calculadora_TDD_Test
 {
+    [TestClass]
     public class CalcTest
     {
+
+    }
+
+}
+
+
+
+
+
+namespace UnitTestProject1
+{
+    
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
     }
 }
